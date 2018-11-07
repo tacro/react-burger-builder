4029993cf24722b7bfe0af370404eddd5d328ca4
Tacro Hashimoto <tacro@g.ucla.edu>
2018-11-08 06:17:58 +0900

    user can add ingredients dynamically;

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import BuildControl from './BuildControl/BuildControl'
+import classes from './BuildControls.module.css'
+
+const controls = [
+  { label: 'Salad', type: 'salad'},
+  { label: 'Bacon', type: 'bacon'},
+  { label: 'Cheese', type: 'cheese'},
+  { label: 'Meat', type: 'meat'},
+];
 
 const buildControls = (props) => (
-  <div>
+  <div className={classes.BuildControls}>
+    {controls.map(ctrl => (
+      <BuildControl
+          key={ctrl.label}
+          label={ctrl.label}
+          added={() => props.ingredientAdded(ctrl.type)} />
+    ))}
   </div>
 );
 
