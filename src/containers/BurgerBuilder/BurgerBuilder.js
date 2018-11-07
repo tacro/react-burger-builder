0d59757280d2eacc411de5dda72534398addff46
Tacro Hashimoto <tacro@g.ucla.edu>
2018-11-08 08:55:34 +0900

    added mordal;

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,7 +26,8 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchaseable: false
+    purchaseable: false,
+    purchasing: false
   }
 
   updatePurchaseState (ingredients) {
@@ -71,6 +72,14 @@ class BurgerBuilder extends Component {
     this.updatePurchaseState(updatedIngredients);
   }
 
+  purchaseHandler = () => {
+    this.setState({purchasing:true});
+  }
+
+  purchaseCancelHandler = () => {
+    this.setState({purchasing:false});
+  }
+
   render () {
     const disabledInfo = {
       ...this.state.ingredients
@@ -80,7 +89,9 @@ class BurgerBuilder extends Component {
     }
     return (
       <>
-        <Modal>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}>
           <OrderSummary ingredients={this.state.ingredients} />
         </ Modal>
         <Burger ingredients={this.state.ingredients} />
@@ -89,6 +100,7 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           purchaseable={this.state.purchaseable}
+          ordered={this.purchaseHandler}
           price={this.state.totalPrice} />
       </>
     );
