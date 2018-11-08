373f413834e48ac573f0749d89eb77894339fa9a
Tacro Hashimoto <tacro@g.ucla.edu>
2018-11-08 11:44:40 +0900

    adding sidebar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
   <>
     <Toolbar />
+    <SideDrawer />
     <div>Toolbar, Sidebar</div>
     <main className={classes.Content}>
       {props.children}
