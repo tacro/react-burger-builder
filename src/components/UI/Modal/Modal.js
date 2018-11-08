97f17f021414949da0511f37aa6b19203182fdb2
Tacro Hashimoto <tacro@g.ucla.edu>
2018-11-08 17:20:19 +0900

    make Modal to class to prevvent unnecessary rerendering in OrderSummary

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <>
-      <Backdrop show={props.show} clicked={props.modalClosed} />
-      <div className={classes.Modal}
-        style={{
-          transform: props.show ? 'transalateY(0)' : 'transalateY(-100vh)',
-          opacity: props.show ? '1' : '0',
-          zIndex: props.show ? '500' : '-100'
-        }}>
-        {props.children}
-      </div>
-    </>
-);
+class Modal extends Component {
 
-export default modal;
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  // componentWillUpdate () {
+  //   console.log('modal willUpdate');
+  // }
+  
+  render() {
+    return (
+      <>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'transalateY(0)' : 'transalateY(-100vh)',
+            opacity: this.props.show ? '1' : '0',
+            zIndex: this.props.show ? '500' : '-100'
+          }}>
+          {this.props.children}
+        </div>
+      </>
+    );
+  };
+}
+
+export default Modal;
