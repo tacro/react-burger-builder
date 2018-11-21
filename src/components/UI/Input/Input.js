c18b26cf90adf116e9e7bf7c22195aace71c7e7e
Tacro Hashimoto <tacro@g.ucla.edu>
2018-11-21 22:57:00 +0900

    adding custom contact form

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,24 @@
+import React from 'raect';
+
+const input = (props) => {
+  let inputElement = null;
+
+  switch(props.inputType) {
+    case('input'):
+      inputElement = <input {...props} />;
+      break;
+    case('textarea'):
+      inputElement = <textarea {...props} />;
+      break;
+    default:
+     inputElement = <input {...props} />;
+  }
+
+  return (
+    <div>
+    <label>{props.label}</label>
+    {inputElement}
+  </div>);
+}
+
+export default input;
