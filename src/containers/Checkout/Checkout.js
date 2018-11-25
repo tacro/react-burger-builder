bfa6892eb4491fd37a04f453728371bc61c4b310
Tacro Hashimoto <tacro@g.ucla.edu>
2018-11-25 10:20:34 +0900

    redirect when order placed

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
+import * as actions from '../../store/actions';
 
 class Checkout extends Component {
 
@@ -18,17 +19,18 @@ class Checkout extends Component {
   render () {
     let summary = <Redirect to='/'/>;
     if (this.props.ings) {
+      const purchasedRedirect = this.props.purchased ?  <Redirect to='/' /> : null;
       summary =(
         <div>
-
-        <CheckoutSummary
-          ingredients={this.props.ings}
-          onCheckCancelled={this.checkoutCancelledHandler}
-          onCheckContinued={this.checkoutContinuedHandler}/>
-          <Route
-            path={this.props.match.path + '/contact-data'}
-            component={ContactData} />
-        </div>
+          {purchasedRedirect}
+          <CheckoutSummary
+            ingredients={this.props.ings}
+            onCheckCancelled={this.checkoutCancelledHandler}
+            onCheckContinued={this.checkoutContinuedHandler}/>
+            <Route
+              path={this.props.match.path + '/contact-data'}
+              component={ContactData} />
+          </div>
       );
     }
     return summary;
@@ -37,8 +39,11 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.burgerBuilder.ingredients
-  }
+    ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased
+  };
 };
+
+
 
 export default connect(mapStateToProps)(Checkout);
