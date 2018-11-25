90703622b19fefc17e2f11ed348d7ae816f44d78
Tacro Hashimoto <tacro@g.ucla.edu>
2018-11-25 10:00:58 +0900

    basically work good on order with redux

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
 import burgerBuildeReducer from './store/reducers/burgerBuilder';
+import orderReducer from './store/reducers/order';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(burgerBuildeReducer, composeEnhancers(
+const rootReducer = combineReducers({
+  burgerBuilder:burgerBuildeReducer,
+  order: orderReducer
+});
+
+const store = createStore(rootReducer, composeEnhancers(
   applyMiddleware(thunk)
 ));
 
